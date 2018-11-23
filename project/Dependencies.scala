17d810af00f355f979ccf93851957a5f48c57a94
Felipe Martins <ffmmjj@gmail.com>
2018-11-23 11:32:22 -0300

    Upgrade Hadoop version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 object Dependencies {
 
   val projectDependencies = Seq(
-    "org.apache.hadoop" % "hadoop-client" % "2.7.3" excludeAll(
+    "org.apache.hadoop" % "hadoop-client" % "2.8.3" excludeAll(
       ExclusionRule("javax.servlet"),
       ExclusionRule("javax.servlet.jsp"),
       ExclusionRule("org.mortbay.jetty"),
